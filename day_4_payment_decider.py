bc3b3dbe85a11f6df72dff8b73cb048eecc9f649
alokr <alokraanjan87@gmail.com>
2024-04-09 18:52:54 +0530

    Payment decider

--- day_4_payment_decider.py
+++ day_4_payment_decider.py
@@ -4,5 +4,6 @@ names = names_string.split(", ")
 # The code above converts the input into an array seperating
 # each name in the input by a comma and space.
 # 🚨 Don't change the code above 👆
+import random
 random_selection=random.randint(0,len(names)-1)
 print(f"{names[random_selection]} is going to buy the meal today!")
