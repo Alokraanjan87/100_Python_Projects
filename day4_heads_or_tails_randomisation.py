585da8b2c311b68d58c02ad41e1c6d7ba0a9fc4e
alokr <alokraanjan87@gmail.com>
2024-04-09 14:02:53 +0530

    Commiting changes to my new repo

---
+++ day4_heads_or_tails_randomisation.py
@@ -0,0 +1,9 @@
+# Please watch a video from Khan Academy which names as PseudoRandom Number Generators
+
+import random
+
+number=random.randint(0,1)
+if number==1:
+    print("Heads")
+else:
+    print("Tails")
